68546b6ad68f494cd0cf5030be37590d04bbfbad
Tharun Rathod <tharunrathod2005@gmail.com>
2026-09-21 14:18:01 +0530

    Add routed application shell for portfolio

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -1,5 +1,17 @@
-import HomePage from './pages/HomePage.jsx';
+import React from 'react';
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
+import HomePage from './pages/HomePage';
 
-export default function App() {
-  return <HomePage />;
+function App() {
+  return (
+    <Router>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
 }
+
+export default App;
